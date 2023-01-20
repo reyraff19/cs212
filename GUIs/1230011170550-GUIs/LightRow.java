6edeb6989c377572368e3551aa8669d7437e257e
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2023-01-20 18:15:53 -0500

    This is the updated LightRow.

--- GUIs/1230011170550-GUIs/LightRow.java
+++ GUIs/1230011170550-GUIs/LightRow.java
@@ -12,6 +12,13 @@ public class LightRow implements ActionListener {
     Container contentPane = window.getContentPane();
     Color[] Colors = { Color.RED, Color.BLUE };
 
+    public Color invert(Color background) {
+        if (background.equals(Color.BLUE)) {
+            return Color.RED;
+        }
+        return Color.BLUE;
+    }
+
     public static void main(String[] args) {
         new LightRow();
     }
@@ -34,22 +41,18 @@ public class LightRow implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("hello");
         JButton source = (JButton) e.getSource();
         for (int i = 0; i < row; i++) {
             if (source == buttons[0]) {
-
                 buttons[0].setBackground(invert(buttons[0].getBackground()));
                 buttons[1].setBackground(invert(buttons[1].getBackground()));
             }
 
             else if (source == buttons[row - 1]) {
-
                 buttons[row - 1].setBackground(invert(buttons[row - 1].getBackground()));
                 buttons[row - 2].setBackground(invert(buttons[row - 2].getBackground()));
 
             } else if (buttons[i] == source) {
-
                 buttons[i].setBackground(invert(buttons[i].getBackground()));
                 buttons[i - 1].setBackground(invert(buttons[i - 1].getBackground()));
                 buttons[i + 1].setBackground(invert(buttons[i + 1].getBackground()));
@@ -59,10 +62,4 @@ public class LightRow implements ActionListener {
         }
     }
 
-    private Color invert(Color background) {
-        if (background.equals(Color.BLUE)) {
-            return Color.RED;
-        }
-        return Color.BLUE;
-    }
 }
