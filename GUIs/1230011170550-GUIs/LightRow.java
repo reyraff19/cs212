dff503f73155a23bc06b0d706ce628d9c9186236
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2023-01-20 21:11:59 -0500

    Commented version of LightRow

--- GUIs/1230011170550-GUIs/LightRow.java
+++ GUIs/1230011170550-GUIs/LightRow.java
@@ -6,13 +6,14 @@ import java.awt.Color;
 public class LightRow implements ActionListener {
 
     JFrame window = new JFrame();
-    String inputValue = JOptionPane.showInputDialog("How many buttons?");
-    int row = Integer.valueOf(inputValue);
+    String inputValue = JOptionPane.showInputDialog("How many buttons?"); // Asks the user how many buttons they wish to
+                                                                          // make.
+    int row = Integer.valueOf(inputValue); // Takes in the user input of how many buttons they want to create.
     JButton[] buttons = new JButton[row];
     Container contentPane = window.getContentPane();
     Color[] Colors = { Color.GREEN, Color.MAGENTA };
 
-    public Color invert(Color background) {
+    public Color invert(Color background) { // Takes in the color of the button and inverts it to be the othre color.
         if (background.equals(Color.GREEN)) {
             return Color.MAGENTA;
         }
@@ -30,29 +31,34 @@ public class LightRow implements ActionListener {
         contentPane.setLayout(new GridLayout());
         for (int i = 0; i < row; i++) {
             JButton button = new JButton();
-            contentPane.add(button);
-            buttons[i] = button;
-            buttons[i].setBackground(Colors[((int) (Math.random() * 2))]);
-            buttons[i].addActionListener(this);
+            contentPane.add(button); // Adds each button to the content pane.
+            buttons[i] = button; // Adds each button that was made to the list of the JButtons.
+            buttons[i].setBackground(Colors[((int) (Math.random() * 2))]); // Sets each button to be a random color,
+                                                                           // between the color green and magenta.
+            buttons[i].addActionListener(this); // When the button is pressed, it will then follow the actionPerformed.
         }
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton source = (JButton) e.getSource();
+        JButton source = (JButton) e.getSource(); // Will retrieve the source of the button that was pressed.
         for (int i = 0; i < row; i++) {
 
-            if (source == buttons[0] && i == 0) {
+            if (source == buttons[0] && i == 0) { // If the first button is pressed, it will invert the color of the
+                                                  // button pressed and the button adjacent to it.
                 buttons[i].setBackground(invert(buttons[0].getBackground()));
                 buttons[i + 1].setBackground(invert(buttons[1].getBackground()));
             }
 
-            else if (source == buttons[row - 1] && i == row - 1) {
+            else if (source == buttons[row - 1] && i == row - 1) { // If the last button is pressed, it will invert the
+                                                                   // color of the last button and the button adjacent
+                                                                   // to it.
                 buttons[row - 1].setBackground(invert(buttons[row - 1].getBackground()));
                 buttons[row - 2].setBackground(invert(buttons[row - 2].getBackground()));
 
-            } else if (buttons[i] == source) {
+            } else if (buttons[i] == source) { // If it is any other button pressed, it will change the color of that
+                                               // button and the buttons adjacent to it.
                 buttons[i].setBackground(invert(buttons[i].getBackground()));
                 buttons[i - 1].setBackground(invert(buttons[i - 1].getBackground()));
                 buttons[i + 1].setBackground(invert(buttons[i + 1].getBackground()));
